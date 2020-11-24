f5fc2d26ceb1f590b6d00cd33d0df73120b81ff4
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-11-24 15:18:15 -0300

    add sql basic seed

---
+++ seed.sql
@@ -0,0 +1,114 @@
+/* Seed alunos */
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 1, 'Aluno1', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 2, 'Aluno2', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 3, 'Aluno3', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 4, 'Aluno4', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 5, 'Aluno5', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 6, 'Aluno6', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 7, 'Aluno7', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 8, 'Aluno8', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 9, 'Aluno9', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 10, 'Aluno10', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 11, 'Aluno11', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 12, 'Aluno12', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 13, 'Aluno13', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 14, 'Aluno14', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 15, 'Aluno15', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 16, 'Aluno16', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 17, 'Aluno17', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 18, 'Aluno18', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 19, 'Aluno19', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 20, 'Aluno20', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 21, 'Aluno21', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 22, 'Aluno22', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 23, 'Aluno23', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 24, 'Aluno24', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 25, 'Aluno25', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 26, 'Aluno26', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 27, 'Aluno27', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 28, 'Aluno28', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 29, 'Aluno29', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 30, 'Aluno30', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 31, 'Aluno31', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+INSERT INTO students (id, matricula, nome, sexo, estado_civil, filiacao, endereco, telefone, email, esporte_preferido, escolaridade, naturalidade, cidade, uf, pais, cargo, cpf, identidade, created_at, updated_at) 
+VALUES (DEFAULT, 32, 'Aluno32', 'Masculino', 'Solteiro', 'Pai1', 'Endereço1', '61 99999999', 'aluno1@email.com', 'Futebol', 'Ensino Médio', 'Brasileiro', 'Bom Jesus da Lapa', 'Bahia', 'Brasil', 'Estudante', '32181242771', '2941564', NOW() , NOW() );
+
+/* Seed Professores */
+INSERT INTO teachers (id, matricula, nome, sexo, estado_civil, filiacao, titulacao, endereco, telefone, cpf, identidade, naturalidade, cidade, uf, pais, email, escolaridade, cargo, created_at, updated_at) VALUES (1,'1', 'Prof1', 'Masculino', 'Casasdo', 'Mãe1', 'Doutor', 'Endereço', '61 999999999', '13787483675', '1234567', 'Espanhol', 'Brasília', 'DF', 'Brasil', 'prof1@email.com', 'Doutor', 'Professor', NOW(), NOW());
+INSERT INTO teachers (id, matricula, nome, sexo, estado_civil, filiacao, titulacao, endereco, telefone, cpf, identidade, naturalidade, cidade, uf, pais, email, escolaridade, cargo, created_at, updated_at) VALUES (2,'2', 'Prof2', 'Masculino', 'Casasdo', 'Mãe2', 'Doutor', 'Endereço', '61 999999999', '13787483675', '1234567', 'Espanhol', 'Brasília', 'DF', 'Brasil', 'prof2@email.com', 'Doutor', 'Professor', NOW(), NOW());
+INSERT INTO teachers (id, matricula, nome, sexo, estado_civil, filiacao, titulacao, endereco, telefone, cpf, identidade, naturalidade, cidade, uf, pais, email, escolaridade, cargo, created_at, updated_at) VALUES (3,'3', 'Prof3', 'Masculino', 'Casasdo', 'Mãe3', 'Doutor', 'Endereço', '61 999999999', '13787483675', '1234567', 'Espanhol', 'Brasília', 'DF', 'Brasil', 'prof3@email.com', 'Doutor', 'Professor', NOW(), NOW());
+INSERT INTO teachers (id, matricula, nome, sexo, estado_civil, filiacao, titulacao, endereco, telefone, cpf, identidade, naturalidade, cidade, uf, pais, email, escolaridade, cargo, created_at, updated_at) VALUES (4,'4', 'Prof4', 'Masculino', 'Casasdo', 'Mãe4', 'Doutor', 'Endereço', '61 999999999', '13787483675', '1234567', 'Espanhol', 'Brasília', 'DF', 'Brasil', 'prof4@email.com', 'Doutor', 'Professor', NOW(), NOW());
+INSERT INTO teachers (id, matricula, nome, sexo, estado_civil, filiacao, titulacao, endereco, telefone, cpf, identidade, naturalidade, cidade, uf, pais, email, escolaridade, cargo, created_at, updated_at) VALUES (5,'5', 'Prof5', 'Masculino', 'Casasdo', 'Mãe5', 'Doutor', 'Endereço', '61 999999999', '13787483675', '1234567', 'Espanhol', 'Brasília', 'DF', 'Brasil', 'prof5@email.com', 'Doutor', 'Professor', NOW(), NOW());
+
+
+
+/* Seed Cursos */
+
+INSERT INTO courses (id, codigo, descricao, horas_aula, vagas_total, vagas_ocupadas, data_inicio, data_fim, teacher_id, created_at, updated_at) VALUES (1, 1, 'Curso1', 90, 30, 0, CURRENT_DATE(), DATE_ADD(CURRENT_DATE(), INTERVAL 20 DAY ), 1, NOW(), NOW());
+INSERT INTO courses (id, codigo, descricao, horas_aula, vagas_total, vagas_ocupadas, data_inicio, data_fim, teacher_id, created_at, updated_at) VALUES (2, 2, 'Curso2', 90, 30, 0, CURRENT_DATE(), DATE_ADD(CURRENT_DATE(), INTERVAL 20 DAY ), 2, NOW(), NOW());
+INSERT INTO courses (id, codigo, descricao, horas_aula, vagas_total, vagas_ocupadas, data_inicio, data_fim, teacher_id, created_at, updated_at) VALUES (3, 3, 'Curso3', 90, 30, 0, CURRENT_DATE(), DATE_ADD(CURRENT_DATE(), INTERVAL 20 DAY ), 3, NOW(), NOW());
+INSERT INTO courses (id, codigo, descricao, horas_aula, vagas_total, vagas_ocupadas, data_inicio, data_fim, teacher_id, created_at, updated_at) VALUES (4, 4, 'Curso4', 90, 30, 0, CURRENT_DATE(), DATE_ADD(CURRENT_DATE(), INTERVAL 20 DAY ), 4, NOW(), NOW());
+INSERT INTO courses (id, codigo, descricao, horas_aula, vagas_total, vagas_ocupadas, data_inicio, data_fim, teacher_id, created_at, updated_at) VALUES (5, 5, 'Curso5', 90, 30, 0, CURRENT_DATE(), DATE_ADD(CURRENT_DATE(), INTERVAL 20 DAY ), 5, NOW(), NOW());
